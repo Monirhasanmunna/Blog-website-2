5979c6002eb6c351ba1ac9710e15a0f77a96c728
munna <monirhasanmunna1@gmail.com>
2022-04-19 02:35:51 +0600

    route group create for admin and author,controller,middleware create,login,registretion route define succesfully

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
@@ -29,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo;
 
     /**
      * Create a new controller instance.
@@ -38,6 +39,13 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
+        if(Auth::check() && Auth::user()->role->id == 1){
+
+            $this->redirectTo = route('admin.dashboard');
+        }else{
+            $this->redirectTo = route('author.dashboard');
+        }
+
         $this->middleware('guest');
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth','admin']],function(){
+
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+
+});
+
+Route::group(['as'=>'author.','prefix'=>'author','namespace'=>'Author','middleware'=>['auth','author']],function(){
+
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+
+});
+
