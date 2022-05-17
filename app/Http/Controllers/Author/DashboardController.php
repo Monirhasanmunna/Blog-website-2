bc33c95f78ca73e1270ee1250b0effb72f724429
munna <monirhasanmunna1@gmail.com>
2022-05-17 22:23:08 +0600

    author dashboard done

--- app/Http/Controllers/Author/DashboardController.php
+++ app/Http/Controllers/Author/DashboardController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers\Author;
 
+use App\Comment;
 use App\Http\Controllers\Controller;
+use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(){
 
-        return view('author.dashboard');
+        $user = Auth::user();
+        $post = Post::publish()->get();
+        $userPost = $user->posts()->publish()->get();
+        $comments = $user->posts()->first();
+        $pendingPost = $user->posts()->where('is_approved', 0)->get();
+        return view('author.dashboard',compact('post','userPost','comments','pendingPost'));
         
        }
 }
